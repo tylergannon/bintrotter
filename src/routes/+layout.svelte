9317556213776ba8df3a1ebc85b30f3d5d2e620c
Tyler Gannon <git@tylergannon.com>
2024-02-22 10:50:08 -0800

    Add header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,18 @@
 <script>
+	import Header from '$lib/components/site/header/Header.svelte';
 	import '../app.postcss';
 </script>
 
-<slot />
+<div class="relative flex min-h-screen flex-col bg-background">
+	<Header />
+	<div class="flex-1">
+		<div class="border-b">
+			<div class="container flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10">
+				<slot />
+			</div>
+
+		</div>
+
+
+	</div>
+</div>
